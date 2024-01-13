834aa8a9f477b37d4ee38e871fd4dd7706036623
Radhika426 <radhikably426@gmail.com>
2024-01-13 19:13:56 +0530

    Updated JDBC Committed

--- src/test/java/miscelleneous/SampleJDBCExecuteUpdate.java
+++ src/test/java/miscelleneous/SampleJDBCExecuteUpdate.java
@@ -2,34 +2,33 @@ package miscelleneous;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import com.mysql.cj.jdbc.Driver;
 
-public class SampleJDBCExecuteQuery {
+public class SampleJDBCExecuteUpdate {
 
 	public static void main(String[] args) throws SQLException {
-		
+
 		//Create driver object and import from MySQL
 		Driver driverRef = new Driver();
-		
+
 		//Step-1: Register the database/driver
 		DriverManager.registerDriver(driverRef);
-		
+
 		//Step-2: Establish the connection with database
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/m4db","root","root");
-		
+
 		//Step-3: issue Create Statement
 		Statement state = con.createStatement();
-		
-		//Step-4: execute a query
-		//executeQuery() is used for Select queries
-		ResultSet result = state.executeQuery("select * from customer_info;");
-		if(result.next()) {
-			System.out.println(result.getString(1)+"-"+result.getString(2)+"-"+result.getString(3));
+
+		//Step-4: execute an update 
+		//executeUpdate() is used for all Non-Select queries- create, insert, update, delete, drop, truncate
+		int result = state.executeUpdate("insert into customer_info values(13,'batman','california');");
+		if(result==1) {
+			System.out.println("Data added");
 		}
-		
+
 		//Step-5: Close the DB
 		con.close();
 
